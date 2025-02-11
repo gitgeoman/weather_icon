2aff66385804891ef28d9d56d1cf161db85cc513
gitgeoman <gitgeoman@gmail.com>
2025-02-11 12:46:47 +0100

    Add logging and utility modules with basic functionality
    Introduced a logging setup in `pass_logging.py` for standardized log outputs. Added `pass_utils.py` with helper functions for parallel processing and database connection handling. Updated `main.py` to initialize logging and execute a basic program flow.

---
+++ pass_logging.py
@@ -0,0 +1,11 @@
+import sys
+import logging
+
+"""Logging"""
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
---
+++ pass_utils.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import, unicode_literals
+import concurrent.futures
+import os
+import sqlalchemy
+import random
+import pandas as pd
+import geopandas as gpd
+from sqlalchemy import create_engine
+from sqlalchemy import Table, MetaData, select, and_  # text
+from geoalchemy2 import functions
+from shapely import wkt
+
+from typing import Callable
+from pass_logging import logger
+
+
+def make_parallel(
+        func: Callable, items: list, workers: int = os.cpu_count() * 2, **kwargs
+) -> list:
+    """przetwarzanie listy  z ThreadPoolExecutor"""
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
+        futures = [executor.submit(func, i, **kwargs) for i in items]
+        return [f.result() for f in concurrent.futures.as_completed(futures)]
+
+
+def connect_to_db(db_name: str, db_user: str, db_password: str, db_host: str) -> sqlalchemy.Engine:
+    return create_engine(f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}")
