8815a56bcec3571a83d0743293a8fcc8904b7c87
gitgeoman <gitgeoman@gmail.com>
2025-02-12 09:41:16 +0100

    Refactor file extraction to return decompressed paths.
    Updated the `extract` method to return the decompressed file path for successful extractions or `None` for failures. Adjusted logging and parallel processing to handle and log only valid decompressed files.

--- weather/weather_extractor.py
+++ weather/weather_extractor.py
@@ -6,6 +6,8 @@ import pandas as pd
 from abc import ABC, abstractmethod
 from datetime import datetime, timezone
 
+import pygrib
+
 from pass_logging import logger
 from pass_utils import make_parallel
 
@@ -21,18 +23,22 @@ class IconEuExtractor(Extractor):
     """Extractor for ICON-EU GRIB2 files."""
 
     def extract(self, file_path, output_directory):
+        """Extracts a single file and returns its decompressed path."""
         output_file_path = os.path.join(output_directory,
                                         os.path.basename(file_path)[:-4])  # Remove .bz2 from file name
 
         if not os.path.exists(file_path):
             logger.error(f"Plik {file_path} nie istnieje.")
-            return  # Exit the method for a missing file
+            return None  # Return None for a missing file
+
         try:
             with bz2.BZ2File(file_path, 'rb') as compressed_file, open(output_file_path, 'wb') as decompressed_file:
                 decompressed_file.write(compressed_file.read())
             logger.info(f"Rozpakowano: {output_file_path}")
+            return output_file_path  # Return the path of successfully decompressed file
         except Exception as e:
             logger.error(f"Błąd podczas rozpakowywania pliku {file_path}: {e}", exc_info=True)
+            return None  # Return None in case of an error
 
 
 if __name__ == "__main__":
@@ -56,6 +62,12 @@ if __name__ == "__main__":
     # --------------------------- EXTRACT  -------------------------------
     extractor = IconEuExtractor()
     logger.info(f"Rozpakowywanie {len(downloaded_files)} plików .bz2...")
-    results = make_parallel(extractor.extract, downloaded_files, output_directory=output_directory)
+
+    # Process files in parallel and remove None values
+    decompressed_files = [
+        result for result in make_parallel(extractor.extract, downloaded_files, output_directory=output_directory)
+        if result is not None
+    ]
+
     logger.info("Rozpakowywanie zakończone!")
-    logger.info(f"Wyniki: {results}")
+    logger.info(f"Rozpakowane pliki: {decompressed_files}")
